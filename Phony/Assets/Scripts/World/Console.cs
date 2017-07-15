39a1e641b51e1fca2d560af4234d2c2b499862ad
Wytchcraft <ksjaculus@gmail.com>
2017-07-15 15:49:44 -0400

    hopefully this doesn't break things

--- Phony/Assets/Scripts/World/Console.cs
+++ Phony/Assets/Scripts/World/Console.cs
@@ -11,25 +11,33 @@ public class Console{
     public static void SetWorld(World w){
         world = w;
     }
-    public static string ReadCommand(string command) {
+    public static string ReadCommand(string command){
         string answer = "Nothing entered";
         string[] parse = command.Split(' ');
         if (parse.Length != 0){
             answer = "Not enough tokens entered.";
             switch (parse[0]){
+                case "getLightLevel":
+                    answer = "Light Level: " + Weather.sunIntensity.ToString();
+                    break;
                 case "getTime":
-                    answer = "Current Time: " + World.worldTime.Time.ToString();
+                    answer = "Current Time: " + World.Time.ToString();
                     break;
                 case "getTimeOfDay":
                     answer = Weather.dt.ToString();
+                    break;
+                case "getRealTime":
+                    answer = "Current Time: " + World.RealTime.ToString();
                     break;
                 case "setTime": //setTime <hour>:<minute>
                     if (parse.Length < 2) break;
                     parse = parse[1].Split(':'); if (parse.Length < 2) break;
                     answer = "Error reading data";
                     int a, b;
-                    if (int.TryParse(parse[0], out a)){
-                        if (int.TryParse(parse[1], out b)){
+                    if (int.TryParse(parse[0], out a))
+                    {
+                        if (int.TryParse(parse[1], out b))
+                        {
                             a = Mathf.Clamp(a, 0, 23);
                             b = Mathf.Clamp(b, 0, 59);
                             world.SetTime(a, b);
--- Phony/Assets/Scripts/World/World.cs
+++ Phony/Assets/Scripts/World/World.cs
@@ -11,54 +11,63 @@ using UnityStandardAssets.CrossPlatformInput;
  * Maintains sun
  */
 
-public struct WorldTime
-{
-	public int Day;
-	public int Hour;
-	public float Time;
-}
-
-public class Weather {
-    public enum DayTime { //dunno if this is the best name for it
+public class Weather{
+    public enum DayTime{ //dunno if this is the best name for it
         Dawn, Morning, Noon, Afternoon, Dusk, Night
     }
-    public enum Sky {
+    public enum Sky{
         Sunny, Rainy, Foggy
     }
-    public class DTT {
+    public class DTT{
         public DayTime dt;
         public float time;
+        public float sunIntensity;
         public DTT() { }
-        public DTT(DayTime DT, float TIME) { dt = DT; time = TIME; }
+        public DTT(DayTime DT, float TIME, float INT){
+            dt = DT;
+            time = TIME;
+            sunIntensity = INT;
+        }
     }
     /// <summary>
     /// Current time of day. Dawn, Morning, etc
     /// </summary>
     public static DayTime dt = DayTime.Dawn;
+    public static float sunIntensity = 1.0f;
     public static Sky sky = Sky.Sunny;
     public static List<DTT> assocTime = new List<DTT>(); //start times
 
-    public Weather (){
-        if (assocTime.Count == 0) {
-            assocTime.Add(new DTT(DayTime.Dawn, 6.00f));
-            assocTime.Add(new DTT(DayTime.Morning, 7.00f));
-            assocTime.Add(new DTT(DayTime.Noon, 11.00f));
-            assocTime.Add(new DTT(DayTime.Afternoon, 13.00f));
-            assocTime.Add(new DTT(DayTime.Dusk, 18.00f));
-            assocTime.Add(new DTT(DayTime.Night, 20.00f));
+    public Weather(){
+        if (assocTime.Count == 0){
+            assocTime.Add(new DTT(DayTime.Dawn, 6.00f, 0.5f));
+            assocTime.Add(new DTT(DayTime.Morning, 7.00f, 0.7f));
+            assocTime.Add(new DTT(DayTime.Noon, 11.00f, 1.0f));
+            assocTime.Add(new DTT(DayTime.Afternoon, 13.00f, 0.9f));
+            assocTime.Add(new DTT(DayTime.Dusk, 18.00f, 0.6f));
+            assocTime.Add(new DTT(DayTime.Night, 20.00f, 0f));
         }
     }
-    
-    public static void SetDayTime() {
+
+    public static void SetDayTime(){
         DayTime newdt = assocTime[assocTime.Count - 1].dt; //last item
-        foreach (DTT dtt in assocTime) {
-            if (World.Time > dtt.time) {
+        foreach (DTT dtt in assocTime){
+            if (World.Time > dtt.time){
                 newdt = dtt.dt;
             } else {
                 break;
             }
         }
         dt = newdt;
+    }
+    public static void SetSunLevel(){
+        int time = assocTime.FindIndex(x => x.dt == dt);
+        int time2 = time + 1;
+        if (time == assocTime.Count - 1){
+            time2 = 0;
+        }
+        else if (time == 0) { return; /*There is an error*/}
+        float a = (World.RealTime - assocTime[time].time) / (assocTime[time2].time - assocTime[time].time);
+        sunIntensity = Mathf.Lerp(assocTime[time].sunIntensity, assocTime[time2].sunIntensity, a);
     }
     /// <summary>
     /// Whether the time is within a specified region.
@@ -67,10 +76,10 @@ public class Weather {
     /// <param name="d2">Later DayTime</param>
     /// <param name="time">Time</param>
     /// <returns></returns>
-    public static bool BetweenTime(DayTime d1, DayTime d2, float time) {
+    public static bool BetweenTime(DayTime d1, DayTime d2, float time){
         float a = assocTime.Find(x => x.dt == d1).time;
         float b = assocTime.Find(x => x.dt == d2).time;
-        if (time >= a && time < b) {
+        if (time >= a && time < b){
             return true;
         } else {
             return false;
@@ -78,26 +87,27 @@ public class Weather {
     }
 }
 
-public class World : MonoBehaviour {
+public class World : MonoBehaviour{
     public List<Abr_Gravity> AllGravitySources;
     public UnityEngine.Light sun_primary;
     public static float GravitationalConstant = 100f;
 
     public static int Day { get { return weekday; } }
     public static float Time { get { return hours * 1.0f + minutes * 0.01f; } }
+    public static float RealTime { get { return hours * 1f + minutes * 0.01f + seconds * 0.01f; } }
     public static int Minute { get { return minutes; } }
     public static int Hour { get { return hours; } }
-	
-	public static WorldTime worldTime;
+
     public static Weather weather;
     private static float time = 0f;
     private static int minutes = 0;
+    private static float seconds = 0;
     private static int hours = 12;
     private static int weekday = 0;
 
     public static bool consoleUp = false;
 
-    public AudioSource audio;
+    public AudioSource m_audio;
     private GameObject consoleWindow;
     private UnityEngine.UI.Text answer;
 
@@ -107,56 +117,48 @@ public class World : MonoBehaviour {
     private float angleSun;
 
     //Has to be initialized before any start function is called
-    void Awake() {
+    void Awake(){
         AllGravitySources = new List<Abr_Gravity>();
     }
-    void Start() {
+    void Start(){
         Initialize();
         UISetup();
     }
-    void Update() {
-		Tick();
-        if (CrossPlatformInputManager.GetButtonDown("Console")) {
-            if (consoleUp) {
-                
-            } else {
-                
-            }
-        }
+    void Update(){
+        Tick();
     }
-	
-	void Tick(){
-		if (UnityEngine.Time.time - time > 1f) {
+
+    void Tick(){
+        seconds = UnityEngine.Time.time - time;
+        if (seconds > 1f){
             minutes++;
             time += 1;
-            if (minutes > 60) {
+            if (minutes > 60){
                 hours++;
                 minutes = 0;
-                if (hours > 24) { 
+                if (hours > 24){
                     hours = 0;
                     weekday++;
-                    if (weekday > 7) {
+                    if (weekday > 7){
                         weekday = 0;
                     }
                 }
             }
             Weather.SetDayTime();
-            UpdateSky();
+            Weather.SetSunLevel();
         }
-		worldTime.Hour = hours;
-		worldTime.Day = Day;
-		worldTime.Time = Time;
-	}
-
+        UpdateSky();
+    }
     #region Gravity
     /// <summary>
     /// Returns the up vector of any point in space using the point gravity sources
     /// </summary>
     /// <param name="r">Rigidbody that needs an up vector</param>
     /// <returns></returns>
-    public Vector3 GetGVector(Rigidbody r) {
+    public Vector3 GetGVector(Rigidbody r){
         Vector3 t = Vector3.zero;
-        foreach(Abr_Gravity g in AllGravitySources){
+        foreach (Abr_Gravity g in AllGravitySources)
+        {
             t += g.getForce(r);
         }
         return -t.normalized;
@@ -167,42 +169,42 @@ public class World : MonoBehaviour {
     /// <param name="position">Position in world space</param>
     /// <param name="mass">Mass of rigidbody</param>
     /// <returns></returns>
-    public Vector3 GetGVector(Vector3 position, float mass) {
+    public Vector3 GetGVector(Vector3 position, float mass){
         Vector3 t = Vector3.zero;
-        foreach (Abr_Gravity g in AllGravitySources) {
+        foreach (Abr_Gravity g in AllGravitySources){
             t += g.getForce(position, mass);
         }
         return t;
     }
     #endregion
 
-    private void Initialize() { 
+    private void Initialize(){
         //get time from save file
         time = UnityEngine.Time.time;
-        audio = GetComponent<AudioSource>();
+        m_audio = GetComponent<AudioSource>();
         Console.SetWorld(this);
         sun_primary = GameObject.FindGameObjectWithTag("Sun").GetComponent<UnityEngine.Light>();
         northPole = new Vector3(-1f, 0.3f, 0f).normalized;
         weather = new Weather();
     }
 
-    private void UpdateSky() {
+    private void UpdateSky(){
         //days from 07:00 to 20:00
-        
-        angleSun = 180 * (hours - 7) / 13;
+
+        angleSun = 180 * (RealTime - 7) / 13;
         angleSun = Mathf.Clamp(angleSun, 0, 180);
-        sun_primary.transform.rotation = Quaternion.AngleAxis(-angleSun,northPole)  * Quaternion.identity;
+        sun_primary.transform.rotation = Quaternion.AngleAxis(-angleSun, northPole) * Quaternion.identity;
 
         //sun_primary.intensity = 5f;
     }
 
-    private void UISetup() {
-        consoleWindow = GameObject.Instantiate((GameObject) Resources.Load("ConsoleInput"));
+    private void UISetup(){
+        consoleWindow = GameObject.Instantiate((GameObject)Resources.Load("ConsoleInput"));
         consoleWindow.SetActive(false);
         consoleWindow.transform.SetParent(GameObject.Find("Canvas").transform);
         consoleWindow.GetComponent<RectTransform>().anchoredPosition = new Vector2(-187, -113);
-        foreach (UnityEngine.UI.Text c in consoleWindow.GetComponentsInChildren<UnityEngine.UI.Text>()) {
-            if (c.gameObject.name.Equals("Answer")) {
+        foreach (UnityEngine.UI.Text c in consoleWindow.GetComponentsInChildren<UnityEngine.UI.Text>()){
+            if (c.gameObject.name.Equals("Answer")){
                 answer = c;
                 break;
             }
@@ -213,15 +215,15 @@ public class World : MonoBehaviour {
         input.onEndEdit = se;
     }
     #region Console
-    public void SetTime(int Hours, int Minutes) {
+    public void SetTime(int Hours, int Minutes){
         hours = Hours; minutes = Minutes;
     }
-    public void SendCommand(string c) {
+    public void SendCommand(string c){
         Debug.Log(c);
         answer.text = Console.ReadCommand(c);
     }
-    public void ChangeConsoleState(bool b) {
-        if (b) {
+    public void ChangeConsoleState(bool b){
+        if (b){
             consoleUp = true;
             consoleWindow.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
