0bc139f109b69c842fdda2d338a1cdc5295770a8
Ferrohound <ferrohound@gmail.com>
2017-07-19 10:31:29 -0400

    Location script for NPC movement & Camerawork

---
+++ Phony/Assets/Scripts/World/Location.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//so we can reference positions within the world for NPC movement
+//and camera pans, etc..
+//create an empty game object and attach this script to it
+
+public class Location : MonoBehaviour {
+	
+	public static Dictionary<string, Transform> Locations = 
+		new Dictionary<string, Transform>();
+	public string Name;
+	
+	void Awake()
+	{
+		Locations[Name] = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
